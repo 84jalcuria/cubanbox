9800b17d24e3c8af4abb0fe8084361a203fc2333
jorge ernesto <84jalcuria@gmail.com>
2021-08-11 01:21:29 -0400

    user auth rect

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import hero from '@/assets/hero.jpg';
 import Title from '@/components/ui/Title';
 
-const profile = () => {
+export default function Profile() {
   const router = useRouter();
   const { user } = useUser();
 
@@ -14,41 +14,38 @@ const profile = () => {
       router.replace('/');
     }
   }, [user]);
-  return (
-    <>
-      <div className='w-full flex flex-col justify-center items-stretch'>
-        {/*Hero Section*/}
-        <section className='relative h-[150px] sm:h-[250px] sm:rounded-t-[30px]'>
-          <Image
-            className='sm:rounded-t-[30px]'
-            src={hero}
-            alt='hero picture'
-            layout='fill'
-            objectFit='cover'
-          />
-          {/*Title*/}
-          <div className='hidden sm:block absolute left-14 top-20'>
-            <Title size={'text-7xl'} />
-          </div>
-        </section>
-        {/*Profile Section*/}
-        <section className='w-full h-[600px] bg-white'>
-          {/*TODO: AVATAR*/}
-          <div className='flex flex-col justify-center items-start space-y-2'>
-            <div className='w-20 h-20 border-gray-900 border-4 rounded-full mt-8 ml-8' />
-            <div className='mt-8 ml-8'>
-              <h1 className='text-gray-900 text-xl font-extrabold uppercase leading-5'>
-                Huracan
-              </h1>
-              <h1 className='text-gray-900 text-sm font-medium tracking-tighter'>
-                84jalcuria@gmail.com
-              </h1>
-            </div>
-          </div>
-        </section>
-      </div>
-    </>
-  );
-};
 
-export default profile;
+  return (
+    <div className='w-full flex flex-col justify-center items-stretch'>
+      {/*Hero Section*/}
+      <section className='relative h-[150px] sm:h-[250px] sm:rounded-t-[30px]'>
+        <Image
+          className='sm:rounded-t-[30px]'
+          src={hero}
+          alt='hero picture'
+          layout='fill'
+          objectFit='cover'
+        />
+        {/*Title*/}
+        <div className='hidden sm:block absolute left-14 top-20'>
+          <Title size={'text-7xl'} />
+        </div>
+      </section>
+      {/*Profile Section*/}
+      <section className='w-full h-[600px] bg-white'>
+        {/*TODO: AVATAR*/}
+        <div className='flex flex-col justify-center items-start space-y-2'>
+          <div className='w-20 h-20 border-gray-900 border-4 rounded-full mt-8 ml-8' />
+          <div className='mt-8 ml-8'>
+            <h1 className='text-gray-900 text-xl font-extrabold uppercase leading-5'>
+              Huracan
+            </h1>
+            <h1 className='text-gray-900 text-sm font-medium tracking-tighter'>
+              84jalcuria@gmail.com
+            </h1>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
